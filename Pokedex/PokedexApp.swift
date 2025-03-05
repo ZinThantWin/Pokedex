5d1811de04dad8aed1cf9fd8171fbe346eef75a1
ZinThantWin <zinthantwin1997@gmail.com>
2025-03-05 15:57:14 +0630

    Initial Commit

---
+++ Pokedex/PokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexApp.swift
+//  Pokedex
+//
+//  Created by Kinzo on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
